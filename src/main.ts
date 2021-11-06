825a4668a12d80fa79539c460247e57138cd0019
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-11-06 18:50:05 +0900

    set orm with env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ConfigService } from '@nestjs/config'; // 追加
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = new ConfigService(); // 追加
+
+  const sync = configService.get('DB_SYNC'); // 追加
+  console.log(`TypeORM synchronize is [ ${sync} ]`); // 追加
+
+  const port = configService.get('API_PORT'); // 追加
+  await app.listen(port); // 3000をportに変更
 }
 bootstrap();
