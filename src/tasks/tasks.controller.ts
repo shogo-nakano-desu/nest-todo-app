e075bb13939404688359b61c221c38b2460c92da
shogo-nakano-desu <61229807+shogo-nakano-desu@users.noreply.github.com>
2021-11-07 12:11:18 +0900

    complete to create REST API. We can check it with curl command

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -12,36 +12,39 @@ import {
 } from '@nestjs/common';
 import { TaskPropertyDto } from './dto/task-property.dto';
 import { TaskStatusPipe } from './pipe/tasks-status.pipe';
+import { TasksService } from './tasks.service';
+import { Task } from './tasks.entity';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
   @Get()
-  getTasks() {
-    return `'getTasks Success!'`;
+  getTasks(): Promise<Task[]> {
+    return this.tasksService.getTasks();
   }
 
   @Get('/:id')
-  getTasksById(@Param('id', ParseIntPipe) id: number) {
-    return `getTaskById Success! Parameter [id:${id}]`;
+  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() taskPropertyDto: TaskPropertyDto) {
+  createTask(@Body() taskPropertyDto: TaskPropertyDto): Promise<Task> {
     const { title, description } = taskPropertyDto;
-    return `createTask Success! Parameter [title:${title}, description:${description}]`;
+    return this.tasksService.createTask(taskPropertyDto);
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id', ParseIntPipe) id: number) {
-    return `deleteTask Success! Parameter [id:${id}]`;
+  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
   }
 
   @Patch('/:id')
   updateTask(
     @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusPipe) status: string,
-  ) {
-    return `updateTask Success! Parameter [id:${id}, status:${status}]`;
+  ): Promise<Task> {
+    return this.tasksService.updateTask(id, status);
   }
 }
